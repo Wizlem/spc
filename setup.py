f5d3c4ab9047cc99475c26ac1b2cb7236ee65153
Wizlem <michael.wisslead@hotmail.com>
2016-02-03 20:09:56 -0600

    Switched to setuptools from distutils.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 setup(name='spc',
       version='0.1',
       description='Load and convert binary spc spectroscopy files',
